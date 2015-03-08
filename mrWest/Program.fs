81f8b08a1aeb8385f7d4a2601b2625d139ea618a
Simo Råman <simoraman@gmail.com>
2015-03-08 17:32:34 +0200

    init

---
+++ mrWest/Program.fs
@@ -0,0 +1,22 @@
+﻿module main
+
+type note = C | Cis | D | Dis | E | F | Fis | G | Gis | A | Ais | B
+
+let frequency = Map.ofList [(C, 261.63); (Cis, 277.18); (D, 293.66); (Dis, 311.13); (E, 329.63);
+                            (F, 349.23); (Fis, 369.99); (G, 392.00); (Gis, 415.30); (A, 440.00);
+                            (Ais, 466.16); (B, 493.88)]
+
+let cMajor = [C; D; E; F; G; A; B]
+
+let randomMelody () = 
+    let r = System.Random()
+    let length = cMajor.Length
+    seq {0 .. 7} |> Seq.map (fun x -> cMajor.[r.Next(0, length)])
+
+[<EntryPoint>]
+let main argv = 
+    randomMelody() 
+        |> Seq.map (fun x -> frequency.[x])
+        |> Synth.writeMelody
+    0 // return an integer exit code
+
---
+++ mrWest/Synth.fs
@@ -0,0 +1,46 @@
+﻿module Synth
+open System.IO
+
+let pack (d:int16[]) = 
+    let stream = new MemoryStream();
+    let writer = new BinaryWriter(stream, System.Text.Encoding.ASCII);
+    let dataLength = Array.length d * 2
+
+    // RIFF
+    writer.Write(System.Text.Encoding.ASCII.GetBytes("RIFF"))
+    writer.Write(Array.length d)
+    writer.Write(System.Text.Encoding.ASCII.GetBytes("WAVE"))
+
+    // fmt
+    writer.Write(System.Text.Encoding.ASCII.GetBytes("fmt "))
+    writer.Write(16)
+    writer.Write(1s)        // PCM
+    writer.Write(1s)        // mono
+    writer.Write(44100)     // sample rate
+    writer.Write(44100 * 16 / 8)     // byte rate
+    writer.Write(2s)        // bytes per sample
+    writer.Write(16s)       // bits per sample
+
+    // data
+    writer.Write(System.Text.Encoding.ASCII.GetBytes("data"))
+    writer.Write(dataLength)
+    let data:byte[] = Array.zeroCreate dataLength
+    System.Buffer.BlockCopy(d, 0, data, 0, data.Length)
+    writer.Write(data)
+    stream
+
+let write (ms:MemoryStream) =
+    use fs = new FileStream(Path.Combine(__SOURCE_DIRECTORY__,"test.wav"), FileMode.Create)
+    ms.WriteTo(fs)
+
+let createNote frequency =
+    let seconds = 0.5
+    let sampleRate = 44100.
+    let requiredSamples = seq { 1.0..(seconds * sampleRate) }
+
+    let samples = Seq.map(fun x -> x |> (*) (2. * System.Math.PI * frequency / 44100.) |> sin |> (*) 32767. |> int16) requiredSamples
+    samples |> Seq.toArray 
+
+let writeMelody melody =
+    let notes = melody |> Seq.map (fun x -> createNote x) |> Seq.reduce (Array.append)
+    notes |> pack |> write
