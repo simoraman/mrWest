c93b66e6ef82df7f78376504fe964ecd96c12ac6
Simo Råman <simoraman@gmail.com>
2015-03-18 20:29:42 +0200

    use markov chain to create melody

--- mrWest/Program.fs
+++ mrWest/Program.fs
@@ -13,10 +13,39 @@ let randomMelody () =
     let length = cMajor.Length
     seq {0 .. 7} |> Seq.map (fun x -> cMajor.[r.Next(0, length)])
 
+let m = [C; C; C; E; D; D; D; F; E; E; D; D; C;
+E; E; E; E; G; F; D; D; D; D; F; E;
+C; C; C; E; D; D; D; F; E; E; D; D; C;]
+
+let w = Seq.windowed 2 m
+
+let grouped = Seq.groupBy (fun (x:note[]) -> x.[0]) w
+
+let lol = Seq.map (fun x -> (fst x, x |> snd |> Seq.map (Seq.nth 1))) grouped
+
+let getNextNote (random:System.Random) (data:seq<note * seq<note>>) (currentNote:note) =
+    let nextSet:seq<note> = data |> Seq.find (fun x -> (fst x) = currentNote) |> snd
+    let nextNoteIndex = random.Next(0, Seq.length nextSet)
+    nextSet |> Seq.skip nextNoteIndex |> Seq.head
+
+let r = System.Random()
+let nextNoteApplied : (seq<note * seq<note>> -> note -> note) = getNextNote r
+let rec randomMelo data current (melo:note list) = 
+    match melo.Length with
+    | 8 -> melo
+    | _ -> 
+        let nextNote = nextNoteApplied data current
+        randomMelo data nextNote (melo @ [nextNote])
+
+let improvedMelody =
+    let r = System.Random()
+    let length = cMajor.Length
+    let firstNote = cMajor.[r.Next(0, length)]
+    randomMelo lol firstNote [firstNote]
+
 [<EntryPoint>]
 let main argv = 
-    randomMelody() 
+    improvedMelody 
         |> Seq.map (fun x -> frequency.[x])
         |> Synth.writeMelody
     0 // return an integer exit code
-
