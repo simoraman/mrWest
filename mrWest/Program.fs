c1e9f386c7bdee6ae9ea198a46e7da7124df0b6b
Simo Råman <simoraman@gmail.com>
2015-03-19 21:10:23 +0200

    format

--- mrWest/Program.fs
+++ mrWest/Program.fs
@@ -1,41 +1,71 @@
 ﻿module main
 
-type note = C | Cis | D | Dis | E | F | Fis | G | Gis | A | Ais | B
+type note = 
+    | C
+    | Cis
+    | D
+    | Dis
+    | E
+    | F
+    | Fis
+    | G
+    | Gis
+    | A
+    | Ais
+    | B
 
-let frequency = Map.ofList [(C, 261.63); (Cis, 277.18); (D, 293.66); (Dis, 311.13); (E, 329.63);
-                            (F, 349.23); (Fis, 369.99); (G, 392.00); (Gis, 415.30); (A, 440.00);
-                            (Ais, 466.16); (B, 493.88)]
+let frequency = 
+    Map.ofList [ (C, 261.63)
+                 (Cis, 277.18)
+                 (D, 293.66)
+                 (Dis, 311.13)
+                 (E, 329.63)
+                 (F, 349.23)
+                 (Fis, 369.99)
+                 (G, 392.00)
+                 (Gis, 415.30)
+                 (A, 440.00)
+                 (Ais, 466.16)
+                 (B, 493.88) ]
 
-let cMajor = [C; D; E; F; G; A; B]
+let cMajor = [ C; D; E; F; G; A; B ]
+let nooaMelody = 
+    [ C; C; C; E; D; D; D; F; E; E; D; D; C; E; E; E; E; G; F; D; D; D; D; F; E; C; C; C; E; D; D; D; F; E; E; D; D; C ]
 
-let nooaMelody = [C; C; C; E; D; D; D; F; E; E; D; D; C;
-E; E; E; E; G; F; D; D; D; D; F; E;
-C; C; C; E; D; D; D; F; E; E; D; D; C;]
+let noteData = 
+    nooaMelody
+    |> Seq.windowed 2
+    |> Seq.groupBy (fun (x : note []) -> x.[0])
+    |> Seq.map (fun x -> 
+           (fst x, 
+            x
+            |> snd
+            |> Seq.map (Seq.nth 1)))
 
-let noteData = nooaMelody 
-            |> Seq.windowed 2 
-            |> Seq.groupBy (fun (x:note[]) -> x.[0])
-            |> Seq.map (fun x -> (fst x, x |> snd |> Seq.map (Seq.nth 1))) 
-
-let getNextNote (random:System.Random) (data:seq<note * seq<note>>) (currentNote:note) =
-    let nextSet:seq<note> = data |> Seq.find (fun x -> (fst x) = currentNote) |> snd
+let getNextNote (random : System.Random) (data : seq<note * seq<note>>) (currentNote : note) = 
+    let nextSet : seq<note> = 
+        data
+        |> Seq.find (fun x -> (fst x) = currentNote)
+        |> snd
+    
     let nextNoteIndex = random.Next(0, Seq.length nextSet)
-    nextSet |> Seq.skip nextNoteIndex |> Seq.head
+    nextSet
+    |> Seq.skip nextNoteIndex
+    |> Seq.head
 
 let r = System.Random()
-let nextNoteFromData : (seq<note * seq<note>> -> note -> note) = getNextNote r
+let nextNoteFromData : seq<note * seq<note>> -> note -> note = getNextNote r
 
-let rec randomMelody wantedLength noteData currentNote (melody:note list) = 
-    if melody.Length = wantedLength 
-    then melody
-    else
+let rec randomMelody wantedLength noteData currentNote (melody : note list) = 
+    if melody.Length = wantedLength then melody
+    else 
         let nextNote = nextNoteFromData noteData currentNote
-        randomMelody wantedLength noteData nextNote (melody @ [nextNote])
+        randomMelody wantedLength noteData nextNote (melody @ [ nextNote ])
 
 [<EntryPoint>]
 let main argv = 
     let firstNote = cMajor.[r.Next(0, cMajor.Length)]
-    randomMelody 8 noteData firstNote [firstNote]
-        |> Seq.map (fun x -> frequency.[x])
-        |> Synth.writeMelody
+    randomMelody 8 noteData firstNote [ firstNote ]
+    |> Seq.map (fun x -> frequency.[x])
+    |> Synth.writeMelody
     0 // return an integer exit code
